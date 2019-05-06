00e08008e489a9f566b09d3c4df67f5490321043
d-w-moore <dmoore@renci.org>
2019-05-06 18:03:49 +0000

    change optimize flag on compiler from 3 to 0

--- Build_cmdline.sh
+++ Build_cmdline.sh
@@ -15,7 +15,7 @@ else
     -Dmsiextract_netcdf_header_EXPORTS -I/opt/irods-externals/clang3.8-0/include/c++/v1 \
     -I/usr/include/irods -I/opt/irods-externals/qpid-with-proton0.34-0/include \
     -I/opt/irods-externals/boost1.60.0-0/include -I/opt/irods-externals/jansson2.7-0/include \
-    -I/opt/irods-externals/libarchive3.3.2-0/include  -O3 -DNDEBUG -nostdinc++ \
+    -I/opt/irods-externals/libarchive3.3.2-0/include  -O0 -DNDEBUG -nostdinc++ \
     -std=c++14 -Wno-write-strings -std=gnu++14 \
     -o lib-microservice-extract_netcdf_header.cpp.o \
     -c ../src/lib-microservice-extract_netcdf_header.cpp -g
@@ -27,7 +27,7 @@ else
 
 LD_RUN=/opt/irods-externals/clang-runtime3.8-0/lib
 
-    /opt/irods-externals/clang3.8-0/bin/clang++ -O3 -DNDEBUG  -stdlib=libc++ \
+    /opt/irods-externals/clang3.8-0/bin/clang++ -O0 -DNDEBUG  -stdlib=libc++ \
     lib-microservice-extract_netcdf_header.cpp.o \
     -lc++abi -lnetcdf -lhdf5 -lz /opt/irods-externals/boost1.60.0-0/lib/libboost_filesystem.so \
     /opt/irods-externals/boost1.60.0-0/lib/libboost_system.so \
